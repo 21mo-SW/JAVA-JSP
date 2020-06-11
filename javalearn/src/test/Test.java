5dfb25236b5a30dbe32ab8296c2c8a1c612579e9
21mo <leewonmo94@gamil.com>
2020-06-12 02:41:07 +0900

    JAVA src

--- javalearn/src/test/Test.java
+++ javalearn/src/test/Test.java
@@ -1,8 +1,33 @@
 package test;
 
+class Car {
+	String option;
+	int window;
+	void drive() {
+		System.out.println("drive");
+	}
+	void stop() {
+		System.out.println("stop");
+	}
+}
+
+class FireEngine extends Car {
+	void water() {
+		System.out.println("water");
+	}
+}
+
+class Ambulance extends Car {
+	void siren() {
+		System.out.println("siren");
+	}
+}
+
 public class Test {
 
 	public static void main(String[] args) {
+		
+		
 		
 	}
 }
